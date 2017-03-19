ec11eb001f70bb193b89b4d9d5b81b97a910709d
MIHAELA\ursum <ursumihaela2009@gmail.com>
2017-03-19 15:52:54 +0100

    changed main structure

--- app/src/main/java/businesscard/android/com/businesscard/MainActivity.java
+++ app/src/main/java/businesscard/android/com/businesscard/MainActivity.java
@@ -1,7 +1,7 @@
 package businesscard.android.com.businesscard;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
